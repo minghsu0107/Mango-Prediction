2e850b81ec06f0d82a38426f242884d482f4718b
ltf0501 <0110420@stu.nknush.kh.edu.tw>
2020-05-20 00:16:23 +0800

    add CNN model, acc ~= 75%

--- read.py
+++ read.py
@@ -1,25 +1,28 @@
-import pandas as pd
 import os
 import csv
 import cv2
 import random
 import time
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 DIR = 'data/'
 filenames = os.listdir('data')
-def MakeLabel(flag):
+def MakeLabel(flag, sz):
     res = []
     with open(DIR + flag + '.csv', 'r') as f:
         reader = csv.reader(f)
         flag = 0
+        cnt = 0
         for line in reader:
             if not flag:
                 flag = 1
                 continue
+            #if cnt >= 5:
+            #    break
+            cnt += 1
             #print(line)
             tmp = [line[0], line[1]]
             tmp[1] = tmp[1].replace('A', '0')
@@ -28,24 +31,9 @@ def MakeLabel(flag):
             tmp[1] = int(tmp[1])
             res.append(tmp)
     x, y = [], []
-    for i in range(5):#range(len(res)):
+    for i in range(len(res)):
         img = mpimg.imread(DIR + res[i][0])
-        new = cv2.resize(img, (800, 800), interpolation = cv2.INTER_LINEAR)
+        new = cv2.resize(img, (sz, sz), interpolation = cv2.INTER_LINEAR)
         x.append(new)
         y.append(res[i][1])
     return x, y
-
-train = MakeLabel('train')
-print(train[0][0].shape)
-
-
-
-#for i in range(100):
-#    print(filenames[i])
-
-#sns.countplot(df['label'], hue = df['label'])
-#plt.show()
-
-#sector = df.groupby('label')
-#print(sector.size())
-
